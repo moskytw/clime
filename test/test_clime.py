165a555f7135c977cb7f2bc567f3b818ba1641ae
Mosky <mosky.tw@gmail.com>
2012-03-22 10:25:49 +0800

    wrote some test

---
+++ test/test_clime.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import clime
+
+class TestClime(unittest.TestCase):
+
+    def test_autotype(self):
+        cases   = ('string', '100', '100.0', None)
+        answers = ('string',  100 ,  100.0 , None)
+        for case, answer in zip(cases, answers):
+            self.assertEqual(clime.autotype(case), answer)
+
+    def test_argspecfromdoc(self):
+
+        fakef = lambda: ''
+
+        docs = [
+            None,
+            '',
+            'abcd',
+            'f1()',
+            'f2(x)',
+            'f3(x, y)',
+            'f4(x[, a])',
+            'f5(x, y[, a])',
+            'f6(x, y[, a[, b]])',
+            'f7([a])',
+            'f8([a[, b]])',
+        ]
+
+        answers = [
+            (None, 0),
+            (None, 0),
+            (None, 0),
+            (None, 0),
+            (['x'], 0),
+            (['x', 'y'], 0),
+            (['x', 'a'], 1),
+            (['x', 'y', 'a'], 1),
+            (['x', 'y', 'a', 'b'], 2),
+            (['a'], 1),
+            (['a', 'b'], 2),
+        ]
+
+        trans = lambda x: (x[0], len(x[-1] or []))
+
+        for doc, answer in zip(docs, answers):
+            fakef.__doc__ = doc
+            self.assertEqual(trans(clime.getargspecfromdoc( fakef )), answer)
+
+
+if __name__ == '__main__':
+    unittest.main()
