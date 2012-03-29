7f05e999ce8f81df08ed1f779bfddbc3dd52990e
Mosky <mosky.tw@gmail.com>
2012-03-30 00:52:19 +0800

    clime.py supports Python file now

--- clime.py
+++ clime.py
@@ -399,10 +399,27 @@ def main(obj=None, defname=None, doc=None):
 
 if __name__ == '__main__':
 
-    def clime(module_name, *args):
-        '''Dynamicly make a module into CLI program.'''
-        module = __import__(module_name)
-        prog = Program(module)
-        prog(sys.argv[2:])
+    def clime(target, *args):
+        '''Dynamicly make a module or Python file into CLI program.
+
+        `target` can be a moudle or Python file path.'''
+
+        module = None
+
+        try:
+            module = __import__(target)
+        except ImportError:
+            pass
+
+        try:
+            module = {}
+            execfile(target, module)
+        except IOError, e:
+            module = None
+            print '%s: %s' % (sys.argv[0], e)
+
+        if module:
+            prog = Program(module)
+            prog(sys.argv[2:])
 
     main({'clime': clime})
