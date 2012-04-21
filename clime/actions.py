8a384048a971f5406eaa5f5ca94614a191a6f539
Mosky <mosky.tw@gmail.com>
2012-04-21 17:41:36 +0800

    add actions.py

---
+++ clime/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from types import NoneType
+
+def store(oval, nval):
+    return nval
+
+def smart(oval, nval):
+    return nval
+
+default = smart
