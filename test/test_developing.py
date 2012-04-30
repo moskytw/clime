9bf85cbe564cab71b1ded26438b44746d94ce6a7
Mosky <mosky.tw@gmail.com>
2012-04-30 22:07:51 +0800

    added a test for developing

---
+++ test/test_developing.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from clime import *
+
+def repeat(s, times=10):
+    ''' Repeat a string n times.
+    
+    -t, --times  repeat n times.'''
+    return x, y
+
+if __name__ == '__main__':
+
+    p = Parser(repeat)
+    print p.parse('string --times=10')
+    print p.parse('string -t=10')
+
