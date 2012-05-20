1055a17dc6e82aaef686bcb5dd1187c9137d2d70
Mosky <mosky.tw@gmail.com>
2012-05-20 22:46:50 +0800

    complete the parse method of ArgSpec

--- clime/ArgSpec.py
+++ clime/ArgSpec.py
@@ -2,13 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from inspect import getdoc
-from .helper import getargspec, getoptmetas, autotype
+from .helper import getargspec, getoptmetas, autotype, smartreducer
+
+class ParseError(Exception): pass
 
 class ArgSpec(object):
+
+    deftype = staticmethod(autotype)
+    metatypes = {'N': int, 'NUM': int}
 
     def __init__(self, func):
         args, vararg, keyword, defvals = getargspec(func)
 
+        self.func = func
         self.args = args
         self.vararg = vararg
         self.keyword = keyword
@@ -21,11 +27,14 @@ class ArgSpec(object):
                 self.mflags.add(arg)
 
         self.bindings = {}
+        self.metavars = {}
         doc = getdoc(func)
         if not doc: return 
 
         args = set(args)
         for optmetas in getoptmetas(doc):
+            for opt, meta in optmetas:
+                self.metavars[opt.lstrip('-')] = meta
             opts, metas = zip(*optmetas)
             opts = set( opt.lstrip('-') for opt in opts )
             target = (opts & args).pop()
@@ -35,37 +44,83 @@ class ArgSpec(object):
 
     def parse(self, rawargs):
 
+        def sepequal(piece):
+            equalsign = piece.find('=')
+            npiece = ''
+            if equalsign != -1:
+                npiece = piece[equalsign+1:]
+                piece = piece[:equalsign]
+            return piece, npiece
+
+        def mktypewrapper(t):
+            def typewrpper(o):
+                try:
+                    return t(o)
+                except ValueError:
+                    raise ParseError("option '%s' must be %s" % (opt, t.__name__))
+            return typewrpper
+
+        def gettype(opt):
+            meta = self.metavars.get(opt, None)
+            t = self.metatypes.get(meta, self.deftype)
+            return mktypewrapper(t)
+
+        def nextarg():
+            if rawargs and not rawargs[0].startswith('-'):
+                return rawargs.pop(0)
+            else:
+                raise ParseError("option '%s' need a value" % opt)
+
         if isinstance(rawargs, str):
             rawargs = rawargs.split()
 
         pargs = []
-        kargs = []
+        kargs = {}
 
         while rawargs:
-            piece = rawargs.pop(0)
-            equalsign = piece.find('=')
-            if equalsign != -1:
-                piece = piece[:equalsign]
-                rawargs.insert(0, piece[equalsign+1:])
+            piece, npiece = sepequal( rawargs.pop(0) )
+            if npiece: rawargs.insert(0, npiece)
+
             plen = len(piece)
             if piece.startswith('-'):
+
                 if plen >= 3 and piece[1] == '-':
-                    key = piece[2:]
-                    key = self.bindings.get(key, key)
+                    # keyword option
+                    opt = piece[2:]
+                    key = self.bindings.get(opt, opt)
                     vals = kargs.setdefault(key, [])
-                    if key not in self.mflags:
-                        vals.append( autotype(rawargs.pop(0)) )
+                    if key in self.mflags:
+                        vals.append( None )
+                    else:
+                        vals.append( gettype(opt)( nextarg() ) )
                     continue
 
                 if plen >= 2:
-                    for i, c in enumerate(piece[1:]):
-                        key = self.bindings.get(c, c)
-                        if c not in self.mflags:
-                            break
-
-                    for c in piece[1:i]:
-                        key = self.bindings.get(c, c)
+                    # letter option
+                    epiece = enumerate(piece); next(epiece)
+                    for i, opt in epiece:
+                        key = self.bindings.get(opt, opt)
                         vals = kargs.setdefault(key, [])
-                        vals.append(None)
+                        if key in self.mflags:
+                            vals.append( None )
+                        else:
+                            if i == plen-1:
+                                val = nextarg() 
+                            else:
+                                val = piece[i+1:]
+                            vals.append( gettype(opt)(val) )
+                            break
+                    continue
 
-                    #TODO
+            # if doesnt start with '-' or length of piece is not enough
+            pargs.append(piece)
+
+        for key, vals in kargs.iteritems():
+            val = reduce(smartreducer, vals, object)
+            kargs[key] = val
+
+        for mflag in self.mflags:
+            if kargs.get(mflag, 0) is None:
+                kargs[mflag] = not self.defaults[mflag]
+
+        return pargs, kargs
