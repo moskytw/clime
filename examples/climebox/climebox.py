0451098a87df29bc7a1c8b19f4799eb9a2a3c519
TAKANO Mitsuhiro <takano32@gmail.com>
2012-09-18 09:23:41 +0900

    handle sys.argv[0] as command name

--- examples/climebox/climebox.py
+++ examples/climebox/climebox.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: noet sts=4:ts=4:sw=4
+# author: takano32 <tak@no32 dot tk>
+#
+
+def clime_dirname(file_name):
+	if suffix == None:
+		pass
+	else:
+		pass
+	print("basename")
+
+def clime_foo():
+	pass
 
 if __name__ == '__main__':
-    import clime.now
+	import clime
+	import sys, os
+	cmdname = os.path.basename(sys.argv[0])
+	print(cmdname)
+	for cmdnames in clime.Program().cmdfs.keys():
+		print(cmdnames)
+	# import clime.now
 
