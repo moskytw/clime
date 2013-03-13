b25be08c95e634a4972b53630f12112da2623049
Mosky <mosky.tw@gmail.com>
2013-03-14 00:32:32 +0800

    refactored cli.py

--- clime/cli.py
+++ clime/cli.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import imp
 from .program import Program
 
 sys.argv[0] = 'clime'
 
 def clime(target, *args, **kargs):
-    '''Dynamicly make a module or Python file into CLI program.
+    '''Make a module or Python file into CLI program.
 
     `target` can be a moudle or Python file path.'''
 
@@ -16,16 +17,7 @@ def clime(target, *args, **kargs):
     try:
         module = __import__(target)
     except ImportError:
+        module = imp.load_source('tmp', target)
 
-        try:
-            module = {}
-            execfile(target, module)
-        except IOError, e:
-            print >> sys.stderr, 'clime: %s' % e
-            return
-
-    if module is not None:
-        prog = Program(module)
-        prog.main(sys.argv[2:])
-    else:
-        assert 'something wrong. you should not go here.'
+    prog = Program(module)
+    prog.main(sys.argv[2:])
