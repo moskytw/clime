c66939f80fec3fedbdc93d62a68a7b9c60689217
Mosky <mosky.tw@gmail.com>
2013-04-22 18:00:03 +0800

    compressed the lines

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from distutils.core import setup
 
-long_description = open('README.rst').read()
-
 from clime import __version__
 
 setup(
     name    = 'clime',
     description = 'Let you convert any module into a multi-command CLI program without any configuration.',
-    long_description = long_description,
+    long_description = open('README.rst').read(),
     version = __version__,
     author  = 'Mosky',
     author_email = 'mosky.tw@gmail.com',
