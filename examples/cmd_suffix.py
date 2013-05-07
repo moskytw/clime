9393665f1d2928ac716d33ba33727dc038deaaca
Mosky <mosky.tw@gmail.com>
2013-05-07 18:14:31 +0800

    added an example for suffix cmd

---
+++ examples/cmd_suffix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def say(word, name=None):
+    if name:
+        print '%s, %s!' % (word, name)
+    else:
+        print '%s!' % word
+
+def hi_cmd(name=None):
+    say('Hi', name)
+
+def hello_cmd(name=None):
+    say('Hello', name)
+
+if __name__ == '__main__':
+    import clime
+    clime.customize(white_pattern=clime.CMD_SUFFIX)
