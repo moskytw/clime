2a43e5792dc70fbdb06227393fefaf19c8e823c1
Mosky <mosky.tw@gmail.com>
2013-09-26 20:11:04 +0800

    removed the doc, because it doesn't use autodoc actually

--- clime/now.py
+++ clime/now.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''It is the simplest way to convert your module into a CLI program:
-
-::
-
-    import clime.now
-
-Actually, this module is equal to execute:
-
-::
-
-    from clime import Program
-    prog = Program()
-    prog.main()
-
-The :py:class:`~clime.core.Program` class provides many options to customize
-your program. It may be a good idea to take a look.
-
-'''
-
 from .core import start as _start
-
 _start()
