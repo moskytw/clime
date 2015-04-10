62ec526a36fec4deb1fc89c775477a536e1f16b3
Mosky <mosky.tw@gmail.com>
2015-04-10 12:21:21 +0800

    adde examples in underscore_param.py

--- examples/underscore_param.py
+++ examples/underscore_param.py
@@ -2,7 +2,25 @@
 # -*- coding: utf-8 -*-
 
 def echo(first, _second, _third='3rd'):
+    '''
+
+    Examples:
+
+    $ py underscore_param.py 1st 2rd --third 3rd
+    underscore_param.py: echo() got an unexpected keyword argument 'third'
+
+    $ py underscore_param.py 1st 2rd ---third 3rd
+    1st 2rd 3rd
+
+    $ py underscore_param.py 1st 2rd --_third 3rd
+    1st 2rd 3rd
+
+    $ py underscore_param.py 1st ---second 2rd --_third 3rd
+    1st 2rd 3rd
+    '''
+
     print first, _second, _third
 
 if __name__ == '__main__':
     from clime import now
+
